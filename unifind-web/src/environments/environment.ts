63433d74f222c73a0465f510cb35e52e5d86cbb0
Justin Chau <justin.jay.chau@gmail.com>
2018-10-25 00:26:07 -0400

    Pages Off

--- unifind-web/src/environments/environment.ts
+++ unifind-web/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyDFskUJWWWWCYyQAJ3O3LOHYBAgQRw8s0A",
     authDomain: "unifind-online.firebaseapp.com",
